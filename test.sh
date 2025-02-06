db3dbd311f83ab646ac2873c9c0b6bf6b742124f
root <2000011006@stu.pku.edu.cn>
2025-02-06 20:51:53 +0800

    Init branch

--- test.sh
+++
@@ -1,5 +0,0 @@
-for branch in $(git for-each-ref --format='%(refname:short)' refs/remotes); do
-  if git show "$branch:deepmd/pt/loss/ep.py" &>/dev/null; then
-    echo "文件存在于分支: $branch"
-  fi
-done
